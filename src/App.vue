89711fee30e7ca5173c405a2230c77b1e2da783c
serjsen <serjsenjob@gmail.com>
2017-04-13 18:52:23 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">Test
+  <customers></customers>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,32 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import
+    Vue from 'vue'
+import App from './App'
+import vueRouter from 'vue-router'
+import vueResource from 'vue-resource'
+import Customers from './components/Customers'
+import About from './components/About'
+
+
+Vue.use(vueResource);
+Vue.use(vueRouter);
+
+const router = new vueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        {path: '/', component: Customers},
+        {path: '/about', component: About},
+    ]
+})
+/* eslint-disable no-new */
+new Vue({
+    router,
+    template: `
+    <div id="app">
+        <router-link to="/about">About</router-link>
+        <router-view> </router-view>
+    <div>
+  `
+}).$mount('#app')
